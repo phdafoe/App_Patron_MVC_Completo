ea716c587513e41864d22817318208bc6ddca28d
phdafoe <phdafoe@gmail.com>
2015-12-21 11:50:54 +0100

    Initial Commit

---
+++ App_Patron_MVC_Completo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  App_Patron_MVC_Completo
+//
+//  Created by User on 21/12/15.
+//  Copyright © 2015 iCologic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
